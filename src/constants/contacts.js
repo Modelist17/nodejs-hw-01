0528bfd0e8ef825751a8e26d85e5854d02a0dd96
Modelist17 <hunteruboat@gmail.com>
2024-07-21 22:48:37 +0200

    Add contact management scripts and functionalities

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,5 +1,8 @@
-// src/constants/contacts.js
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from 'path';
 
-import path from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const PATH_DB = path.join(__dirname, "../db/db.json");
